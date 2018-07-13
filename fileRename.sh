f02b0aefb56796d4d3d4d5bcbecdb774c901e3f3
MXQ8IUN <mark_x_qi@homedepot.com>
2018-07-13 15:14:55 -0400

    committing changes

--- fileRename.sh
+++ fileRename.sh
@@ -2,10 +2,10 @@
 
 # old_name = 'HIST_LNDC_STD_STR_ITEM_M'
 # new_name = 'LNDC_STD_STR_ITEM_M_HIST'
-old_name='TESTING_LOL'
-new_name='LNDC_STD_STR_ITEM_M_HIST'
+old_name='HIST_LNDC_STD_STR_ITEM_MV_M'
+new_name='LNDC_STD_STR_ITEM_MV_M_HIST'
 
-sed_string='s/'$old_name'/'$new_name'/'
+sed_string='s/'$old_name'/'$new_name'/i'
 sed_string_global=$sed_string'g'
 
 # # Rename file strings that match criteria from first to the second phrase
@@ -13,9 +13,10 @@ sed_string_global=$sed_string'g'
 echo $sed_string_global
 
 # How to change text in file in batch (have to be in folder)
-find ./ -type f -name '*' -exec sed -i '' $sed_string_global {} \;
+find . -type f  \( -name '*.sql' -o -name '*.json' -o -name '*.ini' \) -exec sed -i '' $sed_string_global {} \;
 # find ./ -type f -name '*' -exec sed -i '' 's/LNDC_STD_STR_ITEM_M_HIST/TESTING_LOL/g' {} \;
 
 
 # rename filenames in folder + subfolders
 find . -name '*' -exec rename $sed_string -- {} +
+
